119916a680af6467e4d81ed55ff74a5f34289027
Hajk <tunczorx@gmail.com>
2018-04-25 11:56:41 +0200

    Deleted previous project.

---
+++ LearningEnumerationTypesAndCollectionsV0.9/Bird.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LearningEnumerationTypesAndCollectionsV0._9
+{
+    class Bird
+    {
+        public string Name { get; set; }
+        public virtual void Fly()
+        {
+            Console.WriteLine("Frr... frrr ...");
+        }
+        public override string ToString()
+        {
+            {
+                return "Ptak " + Name;
+            }
+        }
+    }
+}
